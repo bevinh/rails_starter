6ad4474fc67a4c36fa9fceca0e25aceaf08818a7
bevin_hernandez <bevin@logicalengine.com>
2024-03-18 15:09:37 -0500

    keep install script correct

--- install.rb
+++ install.rb
@@ -4,6 +4,7 @@ app_name = app_name.downcase.gsub(/\s+/, "_")
 
 puts "Running Installer, please be patient, this may take awhile..."
 Dir.chdir "sites"
+# Must use this in order to install the proper version of node
 IO.popen(". $NVM_DIR/nvm.sh && nvm install 21.7.1") do |io| 
 `rails new "#{app_name}" --database=postgresql -j esbuild --css bootstrap -m ../template.rb`
 `cd "#{app_name}" &&

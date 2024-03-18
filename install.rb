263aa3f4dd8c1f3769449c43db1cb5ee0ff7168c
bevin_hernandez <bevin@logicalengine.com>
2024-03-18 14:38:00 -0500

    change install script

--- install.rb
+++ install.rb
@@ -4,11 +4,10 @@ app_name = app_name.downcase.gsub(/\s+/, "_")
 
 puts "Running Installer, please be patient, this may take awhile..."
 Dir.chdir "sites"
+IO.popen(". $NVM_DIR/nvm.sh && nvm install 21.7.1") do |io| 
 `rails new "#{app_name}" --database=postgresql -j esbuild --css bootstrap -m ../template.rb`
 `cd "#{app_name}" &&
  bundle install &&`
  puts 'Installation Complete'
- Dir.chdir "sites/#{app_name}"
- `foreman start -f Procfile.dev -p 5000`
- puts "Run foreman start -f Procfile.dev -p 5000 to start your server"
- 
+ puts "run `cd sites/#{app_name} && foreman start -f Procfile.dev -p 5000` to start your server"
+end
